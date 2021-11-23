eb69d16069c3e73c72e9981fb14034bfa90460c3
micci-micci <micci184+github@gmail.com>
2021-11-23 16:22:38 +0900

    create login form

--- src/resources/views/login/login_form.blade.php
+++ src/resources/views/login/login_form.blade.php
@@ -1,6 +1,6 @@
 <x-layout>
     <x-slot name="title">
-        Contact page
+        Login page
     </x-slot>
 
     <header>
@@ -13,4 +13,26 @@
             <h1 class="menu_title">Rese</h1>
         </div>
     </header>
+    <main>
+        <form method="post" action="">
+            <div class="login">
+                <div class="login-bar">
+                    <span class="login-box-text">Login</span>
+                </div>
+                <div class="login-container">
+                    <div class="login-input-box">
+                        <span class="material-icons login-icon">email</span>
+                        <input type="text" name="email" placeholder="Email">
+                    </div>
+                    <div class="login-input-box">
+                        <span class="material-icons login-icon">lock</span>
+                        <input type="text" name="password" placeholder="Password">
+                    </div>
+                    <div class="login-input-box-right">
+                        <input type="submit" class="login-btn" value="ログイン">
+                    </div>
+                </div>
+            </div>
+        </form>
+    </main>
 </x-layout>
