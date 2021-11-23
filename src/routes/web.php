2964da9f703df0ae998bc2bdb4d9a0282010f1c0
micci-micci <micci184+github@gmail.com>
2021-11-23 12:10:39 +0900

    mod routes for auth

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,6 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RestaurantController;
+use App\Http\Controllers\AuthController;
+
+// Authentication
+Route::get('/showlogin', [AuthController::class, 'showLogin'])
+    ->name('login.show');
+Route::post('/login', [AuthController::class, 'login'])
+    ->name('login');
 
 Route::get('/', [RestaurantController::class, 'index'])
     -> name('restraunts.index');
