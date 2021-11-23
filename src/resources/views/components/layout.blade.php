c7efa244c93560a87d531521868c5132c19321c7
micci-micci <micci184+github@gmail.com>
2021-11-23 16:22:38 +0900

    create login form

--- src/resources/views/components/layout.blade.php
+++ src/resources/views/components/layout.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $title }}</title>
     <link rel="stylesheet" href="http://localhost:8080/css/reset.css">
+    <link rel="stylesheet" href="http://localhost:8080/css/common.css">
     <link rel="stylesheet" href="http://localhost:8080/css/style.css">
     {{-- Google Material Icons --}}
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
