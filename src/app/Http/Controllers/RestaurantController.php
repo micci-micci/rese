e6ad1344c2ec97a87531c29e2a4a05a1cada96c9
micci-micci <micci184+github@gmail.com>
2021-11-27 22:39:06 +0900

    add relationship settings to the model

--- src/app/Http/Controllers/RestaurantController.php
+++ src/app/Http/Controllers/RestaurantController.php
@@ -3,11 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Restaurant;
+use App\Models\Area;
+use App\Models\Category;
+
 
 class RestaurantController extends Controller
 {
     public function index()
     {
-        return view('restraunts.index');
+        // $restaurants = Restaurant::all();
+        $restaurants = Restaurant::with('area')->get();
+        // $restaurants = Restaurant::with(['area', 'category_id'])->get();
+
+        // dd($restaurants);
+        return view('restaurants.index')
+            ->with(['restaurants' => $restaurants]);
     }
 }
