53daae74fe07bb3d0d612c6aa772edaddc8d69a5
micci-micci <micci184+github@gmail.com>
2021-11-29 21:57:41 +0900

    fix

--- src/app/Http/Controllers/AuthController.php
+++ src/app/Http/Controllers/AuthController.php
@@ -38,7 +38,7 @@ class AuthController extends Controller
     /**
      * @param \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
-    */
+     */
     public function logout(Request $request)
     {
         Auth::logout();
