eac1d72802c91a1ed0ad5aa1f8003d1c11b5cc62
micci-micci <micci184+github@gmail.com>
2021-12-01 09:07:22 +0900

    create base page for mypage

---
+++ src/resources/views/components/mypage.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{{ $title }}</title>
+    <link rel="stylesheet" href="http://localhost:8080/css/reset.css">
+    <link rel="stylesheet" href="http://localhost:8080/css/common.css">
+    <link rel="stylesheet" href="http://localhost:8080/css/mypage.css">
+
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+</head>
+<body>
+    {{ $slot }}
+</body>
+</html>
