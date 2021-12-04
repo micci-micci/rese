657f10a263b96a0f2a8f47d30ab63923883f01a5
micci-micci <micci184+github@gmail.com>
2021-12-04 23:49:18 +0900

    create mypage

--- src/app/Http/Controllers/MypageController.php
+++ src/app/Http/Controllers/MypageController.php
@@ -3,11 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Reservation;
+use App\Models\Favorite;
+use Illuminate\Support\Facades\Auth;
 
 class MypageController extends Controller
 {
     public function mypage()
     {
-        return view('mypage.mypage');
+        $id = Auth::id();
+        $reservations = Reservation::where('user_id', $id)->get();
+        $favorites = Favorite::where('user_id', $id)->get();
+        return view('mypage.mypage')
+            ->with(['reservations' => $reservations])
+            ->with(['favorites' => $favorites]);
     }
 }
