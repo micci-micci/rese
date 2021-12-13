d40d6a575dac34059ebb57b42187263e8fa17bce
micci-micci <micci184+github@gmail.com>
2021-12-13 09:18:45 +0900

    mod favorite deletion in mypage

--- src/resources/views/components/mypage.blade.php
+++ src/resources/views/components/mypage.blade.php
@@ -3,11 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ $title }}</title>
     <link rel="stylesheet" href="http://localhost:8080/css/reset.css">
     <link rel="stylesheet" href="http://localhost:8080/css/common.css">
     <link rel="stylesheet" href="http://localhost:8080/css/mypage.css">
     <script src="{{ mix("js/humberger.js") }}"></script>
+    <script src="{{ mix("js/favorite.js") }}"></script>
 
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 </head>
