558992fb3fb2681dfdf40cf710896b5936d23db5
micci-micci <micci184+github@gmail.com>
2021-12-13 11:56:18 +0900

    add transaction to models

--- src/app/Models/Reservation.php
+++ src/app/Models/Reservation.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Reservation extends Model
 {
@@ -35,8 +36,19 @@ class Reservation extends Model
     // 予約削除
     public static function destroyReserved($id, $request)
     {
-        Reservation::where('user_id', $id)
-        ->where('restaurant_id', $request->restaurant_id)
-        ->delete();
+        // DB::beginTransaction();
+        // try {
+        //     Reservation::where('user_id', $id)
+        //     ->where('restaurant_id', $request->restaurant_id)
+        //     ->delete();
+        //     DB::commit();
+        // } catch (\Exception $e) {
+        //     DB::rollBack();
+        // }
+        DB::transaction(function() use($id, $request) {
+            Reservation::where('user_id', $id)
+            ->where('restaurant_id', $request->restaurant_id)
+            ->delete();
+        });
     }
 }
