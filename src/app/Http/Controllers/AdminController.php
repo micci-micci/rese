ef979b7606e540f4586adc0b46bc233941957808
micci-micci <micci184+github@gmail.com>
2021-12-25 01:42:57 +0900

    add user del func

--- src/app/Http/Controllers/AdminController.php
+++ src/app/Http/Controllers/AdminController.php
@@ -7,8 +7,20 @@ use App\Models\User;
 
 class AdminController extends Controller
 {
-    public function admin()
+    public function show()
     {
-        return view('management.admin');
+        $users = User::all();
+
+        return view('management.admin')
+            ->with(['users' => $users]);
+    }
+
+    public function destroy(Request $request)
+    {
+        $user = $request->only(['id']);
+
+        User::destroyUser($user);
+
+        return redirect('admin');
     }
 }
--- src/resources/views/components/layout.blade.php
+++ src/resources/views/components/layout.blade.php
@@ -10,10 +10,12 @@
     <link rel="stylesheet" href="/css/style.css">
     <link rel="stylesheet" href="/css/restaurant.css">
     <link rel="stylesheet" href="/css/mypage.css">
+    <link rel="stylesheet" href="/css/management.css">
     <script src="{{ mix("js/humberger.js") }}"></script>
     <script src="{{ mix("js/favorite.js") }}"></script>
     <script src="{{ mix("js/reserve.js") }}"></script>
     <script src="{{ mix("js/modal.js") }}"></script>
+    <script src="{{ mix("js/delete.js") }}"></script>
 
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 </head>
